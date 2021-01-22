c7515e922f568209543a87ba678f3aa09370c20b
swetha1917 <76477056+swetha1917@users.noreply.github.com>
2021-01-22 20:14:33 +0530

    created to checks the results
    the programs displays whether the scored mark is eligible for next level

---
+++ task3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int mark;
+   cout<<"Enter the marks scored:";
+   cin>>mark;
+    if(mark<40)
+	{
+		cout<<"YOU HAVE FAILED,BETTER LUCK NEXT TIME";
+		
+		}
+		else
+		{
+			cout<<"WOW! YOU ARE READY FOR NEXT LEVEL,CONGRATS";
+			}	
+			return 0;
+	
+}
